704dd32e353f2fe997871a04ba9386c3efd9b5dc
antonio machado <macchado@users.noreply.github.com>
2017-10-25 14:17:49 -0200

    Update schema.sql

--- resources/sql/schema.sql
+++ resources/sql/schema.sql
@@ -6,5 +6,12 @@ CREATE TABLE notes (
 insert into notes (note) values('one');
 insert into notes (note) values('two');
 insert into notes (note) values('three');
+insert into notes (note) values('four');
+insert into notes (note) values('five');
+insert into notes (note) values('six');
+insert into notes (note) values('seven');
+insert into notes (note) values('eight');
+insert into notes (note) values('nine');
+insert into notes (note) values('ten');
 
 
